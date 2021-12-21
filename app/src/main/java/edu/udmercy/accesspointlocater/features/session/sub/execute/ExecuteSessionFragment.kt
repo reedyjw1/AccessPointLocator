4cbb07a8cdca7bfb55ea86d44b854d709bd958ac
John Reedy <jwr09@yahoo.com>
2021-12-21 17:12:33 -0500

    [ExectueSession] Added functions to get wifi access points based on currently connected network

--- app/src/main/java/edu/udmercy/accesspointlocater/features/session/sub/execute/ExecuteSessionFragment.kt
+++ app/src/main/java/edu/udmercy/accesspointlocater/features/session/sub/execute/ExecuteSessionFragment.kt
@@ -1,5 +1,9 @@
 package edu.udmercy.accesspointlocater.features.session.sub.execute
 
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.content.IntentFilter
 import android.graphics.Bitmap
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -13,6 +17,7 @@ import com.davemorrissey.labs.subscaleview.ImageSource
 import edu.udmercy.accesspointlocater.R
 import kotlinx.android.synthetic.main.fragment_execute_session.*
 import android.graphics.PointF
+import android.net.wifi.WifiManager
 import android.util.Log
 
 import android.view.MotionEvent
@@ -21,6 +26,8 @@ import android.view.GestureDetector
 import android.view.GestureDetector.SimpleOnGestureListener
 import edu.udmercy.accesspointlocater.arch.BaseFragment
 import edu.udmercy.accesspointlocater.arch.CircleViewPointListener
+import java.util.concurrent.ExecutorService
+import java.util.concurrent.Executors
 
 
 class ExecuteSessionFragment: BaseFragment(R.layout.fragment_execute_session), CircleViewPointListener {
@@ -31,9 +38,22 @@ class ExecuteSessionFragment: BaseFragment(R.layout.fragment_execute_session), C
         private const val TAG = "ExecuteSessionFragment"
     }
 
+    private lateinit var wifiManager: WifiManager
+
+    private val wifiScanReceiver = object : BroadcastReceiver() {
+
+        override fun onReceive(context: Context, intent: Intent) {
+            val success = intent.getBooleanExtra(WifiManager.EXTRA_RESULTS_UPDATED, false)
+            if (success) {
+                scanSuccess()
+            } else {
+                scanFailure()
+            }
+        }
+    }
+
     private val imageObserver =
         Observer { bitmap: Bitmap? ->
-            Log.i(TAG, "bitmap: ${bitmap?.byteCount}")
             if(bitmap != null) {
                 executeImageView.setImage(ImageSource.bitmap(bitmap))
             }
@@ -50,6 +70,34 @@ class ExecuteSessionFragment: BaseFragment(R.layout.fragment_execute_session), C
         val uuid = arguments?.getString("uuid") ?: return
         viewModel.getCurrentSession(uuid)
         executeImageView.listener = this
+        startScanBtn.setOnClickListener {
+            startScan()
+        }
+
+    }
+
+    private fun startScan() {
+        wifiManager = requireActivity().getSystemService(Context.WIFI_SERVICE) as WifiManager
+        val intentFilter = IntentFilter()
+        intentFilter.addAction(WifiManager.SCAN_RESULTS_AVAILABLE_ACTION)
+        requireActivity().registerReceiver(wifiScanReceiver, intentFilter)
+
+        val success = wifiManager.startScan()
+        if (!success) {
+            scanFailure()
+        }
+    }
+
+    private fun scanSuccess() {
+        requireActivity().unregisterReceiver(wifiScanReceiver)
+        val results = wifiManager.scanResults
+        val wifiName = wifiManager.connectionInfo.ssid.toString()
+        val filteredResults = results.filter {"\"" +  it.SSID + "\""== wifiName}
+        val uuid = arguments?.getString("uuid") ?: return
+        viewModel.saveResults(filteredResults, uuid)
+    }
+
+    private fun scanFailure() {
 
     }
 
@@ -59,7 +107,6 @@ class ExecuteSessionFragment: BaseFragment(R.layout.fragment_execute_session), C
         viewModel.getCurrentSession(uuid)
         viewModel.currentBitmap.observe(this, imageObserver)
         viewModel.allowedNumberOfPoints.observe(this, numberOfPointsObserver)
-        Log.i(TAG, "onResume: resuming")
     }
 
     override fun onPause() {
--- app/src/main/java/edu/udmercy/accesspointlocater/features/session/sub/execute/ExecuteSessionViewModel.kt
+++ app/src/main/java/edu/udmercy/accesspointlocater/features/session/sub/execute/ExecuteSessionViewModel.kt
@@ -2,6 +2,7 @@ package edu.udmercy.accesspointlocater.features.session.sub.execute
 
 import android.graphics.Bitmap
 import android.graphics.PointF
+import android.net.wifi.ScanResult
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
@@ -10,13 +11,16 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import org.koin.core.KoinComponent
 import org.koin.core.inject
+import kotlin.math.abs
+import kotlin.math.log10
+import kotlin.math.pow
 
 class ExecuteSessionViewModel: ViewModel(), KoinComponent {
     private val sessionRepo: SessionRepository by inject()
     val currentBitmap: MutableLiveData<Bitmap> = MutableLiveData<Bitmap>()
     val sessionName: MutableLiveData<String> = MutableLiveData()
     var currentPosition: PointF? = null
-    val allowedNumberOfPoints = MutableLiveData<Int>(2)
+    val allowedNumberOfPoints = MutableLiveData<Int>(1)
 
     fun getCurrentSession(uuid: String) {
         viewModelScope.launch(Dispatchers.IO) {
@@ -25,5 +29,20 @@ class ExecuteSessionViewModel: ViewModel(), KoinComponent {
             sessionName.postValue(session.sessionLabel)
 
         }
+    }
+
+    fun saveResults(list: List<ScanResult>, uuid: String) {
+        viewModelScope.launch(Dispatchers.IO) {
+            list.forEach {
+                val distance = calculateDistanceInMeters(it.level, it.frequency)
+                val session = sessionRepo.getCurrentSession(uuid)
+            }
+        }
+    }
+
+    private fun calculateDistanceInMeters(signalLevelInDb: Int, freqInMHz: Int): Double {
+        val exp = (27.55 - 20 * log10(freqInMHz.toDouble()) + abs(signalLevelInDb)) / 20.0
+        val dist = 10.0.pow(exp)
+        return (dist *100.0 ) / 1000.0
     }
 }
