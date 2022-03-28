e13edb4e6fff6b90192737242e07c97f91667ea6
John Reedy <jwr09@yahoo.com>
2022-03-28 10:27:12 -0400

    Fixed issue with reading DB on main thread

--- app/src/main/java/edu/udmercy/accesspointlocater/features/testDistance/view/DistanceTestViewModel.kt
+++ app/src/main/java/edu/udmercy/accesspointlocater/features/testDistance/view/DistanceTestViewModel.kt
@@ -5,9 +5,12 @@ import android.net.wifi.ScanResult
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.viewModelScope
 import edu.udmercy.accesspointlocater.features.accessPointChooser.repositories.AccessPointReferenceRepository
 import edu.udmercy.accesspointlocater.features.testDistance.model.DistanceUI
 import edu.udmercy.accesspointlocater.utils.MathUtils
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 import org.koin.core.KoinComponent
 import org.koin.core.inject
 
@@ -21,23 +24,25 @@ class DistanceTestViewModel(
     var isRunning = false
 
     fun updateList(list: List<ScanResult>) {
-        val uuid = savedStateHandle.getLiveData<String>("uuid").value ?: return
-        val ap = apChooser.getReferenceAccessPoint(uuid)
+        viewModelScope.launch(Dispatchers.IO) {
+            val uuid = savedStateHandle.getLiveData<String>("uuid").value ?: return@launch
+            val ap = apChooser.getReferenceAccessPoint(uuid)
 
-        val temp = list.map {
-            DistanceUI(
-                it.BSSID,
-                it.level,
-                it.frequency,
-                MathUtils.calculateDistanceInMeters(
-                    it.level, 3.0, ap.distance, ap.level.toDouble()
+            val temp = list.map {
+                DistanceUI(
+                    it.BSSID,
+                    it.level,
+                    it.frequency,
+                    MathUtils.calculateDistanceInMeters(
+                        it.level, 3.0, ap.distance, ap.level.toDouble()
+                    )
                 )
-            )
-        }.toMutableList()
+            }.toMutableList()
 
-        temp.sortByDescending { it.rssi }
-        accessPointList.postValue(
-            temp
-        )
+            temp.sortByDescending { it.rssi }
+            accessPointList.postValue(
+                temp
+            )
+        }
     }
 }
