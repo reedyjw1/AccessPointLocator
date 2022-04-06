9523a786d5cf763d73605137c69a87068e9cbdfe
Stephen Reedy <reedysm@udmercy.edu>
2022-04-06 10:45:01 -0400

    initialized image view with first floor image

--- app/src/main/java/edu/udmercy/accesspointlocater/features/placeAccessPoints/view/PlaceAccessPointsFragment.kt
+++ app/src/main/java/edu/udmercy/accesspointlocater/features/placeAccessPoints/view/PlaceAccessPointsFragment.kt
@@ -3,23 +3,52 @@ package edu.udmercy.accesspointlocater.features.placeAccessPoints.view
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.Observer
+import com.davemorrissey.labs.subscaleview.ImageSource
 import edu.udmercy.accesspointlocater.R
 import edu.udmercy.accesspointlocater.arch.BaseFragment
+import edu.udmercy.accesspointlocater.features.create.room.BuildingImage
+import kotlinx.android.synthetic.main.fragment_place_access_points.*
+import kotlinx.android.synthetic.main.fragment_view_session.*
 
 class PlaceAccessPointsFragment : BaseFragment(R.layout.fragment_place_access_points) {
+
+
+    // Things to do
+    // load in images for correct session
+    // hookup buttons to move between floor images
+    // allow for multiple placements per floor
+    // save placements as AP location object for session
+
 
     companion object {
         private const val TAG = "PlaceAccessPointsFragment"
     }
 
-
     private val viewModel by viewModels<PlaceAccessPointsViewModel>()
 
+    private val imageObserver =
+        Observer { bitmap: BuildingImage? ->
+            if(bitmap != null) {
+                apLocationPlacer.setImage(ImageSource.bitmap(bitmap.image))
+            }
+        }
+
+    
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         val uuid = arguments?.getString("uuid") ?: return
+        viewModel.initializeImages(uuid)
     }
 
+    override fun onPause() {
+        super.onPause()
+        viewModel.currentDisplayImage.removeObserver(imageObserver)
+    }
 
+    override fun onResume() {
+        super.onResume()
+        viewModel.currentDisplayImage.observe(this, imageObserver)
+    }
 
 }
--- app/src/main/java/edu/udmercy/accesspointlocater/features/placeAccessPoints/view/PlaceAccessPointsViewModel.kt
+++ app/src/main/java/edu/udmercy/accesspointlocater/features/placeAccessPoints/view/PlaceAccessPointsViewModel.kt
@@ -1,8 +1,32 @@
 package edu.udmercy.accesspointlocater.features.placeAccessPoints.view
 
+import android.graphics.Bitmap
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import edu.udmercy.accesspointlocater.features.create.repositories.BuildingImageRepository
+import edu.udmercy.accesspointlocater.features.create.room.BuildingImage
+import edu.udmercy.accesspointlocater.features.home.repositories.SessionRepository
+import edu.udmercy.accesspointlocater.features.viewSession.repositories.APLocationRepository
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 import org.koin.core.KoinComponent
+import org.koin.core.inject
 
 class PlaceAccessPointsViewModel : ViewModel(), KoinComponent {
+
+    private val sessionRepo: SessionRepository by inject()
+    private val buildingImageRepo: BuildingImageRepository by inject()
+    private val accessPointRepo: APLocationRepository by inject()
+
+    val currentDisplayImage: MutableLiveData<BuildingImage> = MutableLiveData()
+
+    fun initializeImages(uuid: String){
+        viewModelScope.launch(Dispatchers.IO) {
+            val session = sessionRepo.getCurrentSession(uuid)
+            val firstFloorImage = buildingImageRepo.getFloorImage(uuid, 0)
+            currentDisplayImage.postValue(firstFloorImage)
+        }
+    }
 
 }
